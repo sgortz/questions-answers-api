2d72c64e213a3c904ab8b482c899a9379bef02e1
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-04-25 19:05:58 -0400

    Working on the POST requests

--- database/db.js
+++ database/db.js
@@ -10,10 +10,11 @@ const pool = new Pool({
   database: process.env.DB
 })
 
+
 /* Create a function to return all questions, answers and answer photos from a product id. 
  * Filter out reported questions or answers  */
 function questionsList(product_id, limit) {
-  return pool.query(`SELECT * FROM public.questions WHERE product_id = $1 LIMIT $2`, [product_id, limit]);
+  return pool.query(`SELECT * FROM questions WHERE product_id = $1 LIMIT $2`, [product_id, limit]);
   // figure out how to query for just one product id
   // filter out reported questions and answers
 }
@@ -23,13 +24,13 @@ function answersList(question_id, limit) {
   // insert page property (here?)
   // insert count property (here?)
   // remove all reported answers
-  return pool.query(`SELECT * FROM public.answers WHERE question_id = $1 LIMIT $2`, [question_id, limit]);
+  return pool.query(`SELECT * FROM answers WHERE question_id = $1 LIMIT $2`, [question_id, limit]);
 }
 
 function insertQuestion(body, name, email, product_id, date) {
   return pool.query(`
-    INSERT INTO public.questions (product_id, body, date_written, asker_name, asker_email) 
-    VALUES(${product_id},${body},${date},${name},${email})
+    INSERT INTO questions (product_id, body, date_written, asker_name, asker_email) 
+    VALUES('${product_id}',${body},${date},${name},${email});
     `)
 }
 
@@ -40,7 +41,8 @@ function insertAnswer(body, name, email, question_id, date) {
     `)
 }
 
-function test(product_id) {
+function testForGet(product_id) {
   return pool.query(`SELECT * FROM questions WHERE product_id = '${product_id}' LIMIT 3 `);
 }
-module.exports = { pool, test, questionsList, answersList, insertQuestion, insertAnswer };
+
+module.exports = { pool, testForGet, questionsList, answersList, insertQuestion, insertAnswer };
--- server/models.js
+++ server/models.js
@@ -1,9 +1,50 @@
-const { pool, questions } = require('../database/db.js');
+const { testForGet, questionsList, answersList, insertQuestion, insertAnswer } = require('../database/db.js');
 
 module.exports = {
-    getQuestions: function(product_id, page, count) {
-        let limit = page * count;
-        questions(limit).then(response => console.log('does this work? ', response.rows))
+  getQuestions: function (product_id, page, count, callback) {
+    let limit = page * count;
+    return testForGet(product_id)
+    // return questionsList(product_id, limit)
+      .then(response => {
+        callback(null, response.rows)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },
+
+  getAnswers: function (product_id, page, count) {
+    let limit = page * count;
+    answersList(product_id, limit)
+      .then(response => {
+        console.log('response from DB for answers ', response.rows)
         // return response
-    }
+      })
+  },
+  
+  addQuestion: function (data) {
+    let { body, name, email, product_id } = data;
+
+    let timestamp = Date.now();
+
+    return insertQuestion(body, name, email, product_id, timestamp)
+      .then(response => {
+        console.log('response from DB: ', response)
+        callback(null, 'Created');
+      })
+      .catch(err => {
+        console.error(err)
+      });
+  },
+
+  addAnswer: function (data) {
+    let { body, name, email, question_id } = data;
+    let timestamp = Date.now();
+    insertAnswer(body, name, email, question_id, timestamp)
+      .then(response => {
+        console.log('response from DB: ', response)
+        // return this response as a promise
+      })
+      .catch(err => console.error(err));
+  },
 }
